eb65974a71648dbae7c1e5ea9fd02340901206f7
= <=>
2025-02-15 23:19:27 +0530

    Typography Component implmented

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Typography from './components/common/Typography'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Typography variant='2xl' >Vite + React</Typography>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/components/common/Typography/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+/**
+ * Typography component for rendering text with Tailwind variants.
+ * 
+ * @param {Object} props - Component properties.
+ * @param {keyof JSX.IntrinsicElements} [props.component="p"] - The HTML tag to render (e.g., 'p', 'h1', 'span').
+ * @param {'base' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl'} [props.variant="base"] - Tailwind text size variant.
+ * @param {string} [props.className=""] - Additional CSS classes for styling.
+ * @param {React.ReactNode} [props.children=null] - Content inside the component.
+ * @returns {JSX.Element} A React component with the specified typography styling.
+ */
+const Typography = ({ component: Component = 'p', variant = 'base', children = null, className = "" }) => {
+
+    const _variant = getVariant(variant);
+    const _className = `${_variant} ${className}`;
+
+    return <Component className={_className}>{children}</Component>;
+};
+
+export default Typography;
+
+function getVariant(variant = 'base') {
+    switch (variant) {
+        case 'base': return 'text-base';
+        case 'lg': return 'text-lg';
+        case 'xl': return 'text-xl';
+        case '2xl': return 'text-2xl';
+        case '3xl': return 'text-3xl';
+        case '4xl': return 'text-4xl';
+        case '5xl': return 'text-5xl';
+        default: return 'text-base';
+    }
+}
