d06d6be2650e00280c2166a411fe7844eb6d9f3b
= <=>
2025-02-18 20:49:11 +0530

    TextLoop Animation improved

--- src/components/TextLoop.jsx
+++ src/components/TextLoop.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { AnimatePresence, motion, transform } from "framer-motion";
+import { AnimatePresence, delay, motion, transform } from "framer-motion";
 import Headline from "./common/Headline";
 
 const variants = {
@@ -18,8 +18,9 @@ const variants = {
     }),
 };
 
-const TextLoop = ({ texts, interval = 3000 }) => {
+const TextLoop = ({ texts = [], interval = 3000 }) => {
     const [index, setIndex] = useState(0);
+    const length = texts[index].length
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -37,24 +38,28 @@ const TextLoop = ({ texts, interval = 3000 }) => {
                     {/* <div className="h-2 w-full absolute bg-amber-700 left-0 top-0"></div> */}
                     <Headline variant="h1" className="absolute left-[50%] top-0 w-full !block" style={{
                         transform: 'translateX(-50%)'
-                    }} >
-                        <motion.span
-                            className="absolute top-0 left-0 w-full"
-                            style={{
-                                // transform: 'translateX(50%)'
-                            }}
-                            variants={variants}
-                            key={index}
-                            initial="enter"
-                            animate="center"
-                            exit="exit"
-                            transition={{
-                                y: { type: "spring", stiffness: 300, damping: 200 },
-                                opacity: { duration: 0.5 },
-                            }}
-                        >
-                            {texts[index]}
-                        </motion.span>
+                    }}
+                        key={index}
+                    >
+                        {texts[index].split("").map((word, index) =>
+                            <motion.span
+                                className="relative top-0 left-0 w-full"
+                                variants={variants}
+                                key={index}
+                                initial="enter"
+                                animate="center"
+                                exit="exit"
+                                transition={{
+                                    y: { type: "spring", stiffness: 300, damping: 200 },
+                                    opacity: { duration: 0.5, delay: index / 10 },
+                                    delay: 0.4 * index
+                                }}
+                            >
+                                {/* {texts[index]} */}
+                                {word}
+                            </motion.span>
+                        )}
+
                     </Headline>
                 </div>
 
