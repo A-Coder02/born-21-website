ec6497a97e278c24d65b54e464470cac7e8ea6d2
= <=>
2025-02-22 13:18:53 +0530

    responsive appbar implemented

--- src/components/layout-ui/Appbar/index.jsx
+++ src/components/layout-ui/Appbar/index.jsx
@@ -14,7 +14,7 @@ const Appbar = () => {
     const isHeaderToggleStyles = useScrollChangeAppBar();
     const navigate = useNavigate();
     return (
-        <Section containerClassName='!py-8 !px-32' sectionClassName='fixed top-0 left-0 z-50' style={{
+        <Section containerClassName='!py-5 md:!py-8 md:!px-32' sectionClassName='fixed top-0 left-0 z-50' style={{
             backgroundColor: !isHeaderToggleStyles ? 'white' : 'transparent',
             borderColor: !isHeaderToggleStyles ? 'black' : 'transparent',
             borderBottomWidth: '1px',
@@ -23,9 +23,9 @@ const Appbar = () => {
             <div className={"header-wrapper flex items-center justify-between transition-all "}
             >
 
-                <div className={`brand-wrapper flex gap-1 text-${isHeaderToggleStyles ? 'white' : "black"}`}>
-                    <img src={BrandPng} onClick={() => navigate('/')} className='w-28 cursor-pointer' alt="born-21" />
-                    <span className="brand-name flex flex-col font-nohemi">
+                <div className={`brand-wrapper flex gap-1 items-center text-${isHeaderToggleStyles ? 'white' : "black"}`}>
+                    <img src={BrandPng} onClick={() => navigate('/')} className='w-16 object-contain md:w-28 cursor-pointer' alt="born-21" />
+                    <span className="brand-name flex flex-col font-nohemi text-[0.5rem] md:text-base">
                         <span>
                             BORN
                         </span>
@@ -40,16 +40,16 @@ const Appbar = () => {
 
                 <nav className={`nav-wrapper flex gap-4 items-center text-${isHeaderToggleStyles ? 'white' : "black"}`}>
                     {/* <Typography variant='4xl' className={`font-medium cursor-pointer !pr-4 border-r border-r-${isHeaderToggleStyles ? 'white' : "black"}`} >Menu</Typography> */}
-                    <Typography
-                        variant='4xl'
-                        className='group font-medium  cursor-pointer  inline-flex gap-3 items-center'
+                    <div
+                        // variant='4xl'
+                        className='t-base md:t-4xl group font-medium  cursor-pointer  inline-flex gap-3 items-center'
                     >
                         <span className="relative pb-1 before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] before:bg-white before:transition-all before:duration-300 group-hover:before:w-full">
                             Start a project
                         </span>
 
-                        <img src={VectorPng} className='w-5 h-5 transition-transform duration-300 group-hover:rotate-45' />
-                    </Typography>
+                        <img src={VectorPng} className='w-3 h-3 md:w-5 md:h-5 transition-transform duration-300 group-hover:rotate-45' />
+                    </div>
 
 
                 </nav>
--- src/components/layout-ui/Section/index.jsx
+++ src/components/layout-ui/Section/index.jsx
@@ -13,7 +13,7 @@ import React from 'react'
 const Section = ({ style = {}, children = null, sectionClassName = '', containerClassName = '', id = null }) => {
     return (
         <section className={`w-full ${sectionClassName}`} style={style} id={id} >
-            <div className={`${containerClassName} !px-8 !mx-auto max-w-[120rem]`} >
+            <div className={`px-4 ${containerClassName}  !mx-auto max-w-[120rem]`} >
                 {children}
             </div >
         </section >
