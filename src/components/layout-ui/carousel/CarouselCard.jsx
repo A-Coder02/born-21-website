a832a12a82337335d733c1647fb2c75970e090d7
Arbaj Ansari <arbaj897ansari@gmail.com>
2025-02-23 18:16:00 +0530

    made changes inCarouselCard in styles

--- src/components/layout-ui/carousel/CarouselCard.jsx
+++ src/components/layout-ui/carousel/CarouselCard.jsx
@@ -1,7 +1,7 @@
 import Typography from "../../common/Typography";
 
 const CarouselCard = ({ theme = {}, card, className = '', style = null }) => {
-    return <article style={style} className={`${theme?.cardClassName?.container} flex flex-col  py-5 px-8 rounded-4xl flex-[0_0_24rem] min-h-96 h-[24rem] md:h-[30rem] ${className} transition-all duration-[1000ms] ease-[cubic-bezier(0.25,1.25,0.5,1)]`}>
+    return <article style={style} className={`${theme?.cardClassName?.container} flex flex-col  py-5 px-8 rounded-4xl flex-[0_0_24rem] min-h-96 h-[26rem] md:h-[30rem] ${className} transition-all duration-[1000ms] ease-[cubic-bezier(0.25,1.25,0.5,1)]`}>
         <p className={'md:h-20  !text-6xl md:!text-[5rem] font-black ' + theme?.cardClassName?.title} >“</p>
         <p className={'font-medium mb-3 md:text-lg ' + theme?.cardClassName?.title}>{card?.title}</p>
         <h5 className="mb-4 text-sm md:text-base" >{card.content}</h5>
