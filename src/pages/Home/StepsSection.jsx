e5769fdfa4eee50b715656415712e76aa45b356c
Arbaj Ansari <arbaj897ansari@gmail.com>
2025-02-26 21:27:31 +0530

    Steps Section UI modified

--- src/pages/Home/StepsSection.jsx
+++ src/pages/Home/StepsSection.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import Section from '../../components/layout-ui/Section'
 
-import Step1Svg from '../../assets/svgs/steps/step-1.svg';
-import Step2Svg from '../../assets/svgs/steps/step-2.svg';
-import Step3Svg from '../../assets/svgs/steps/step-3.svg';
+import Step1Svg from '../../assets/svgs/steps/step_1.svg';
+import Step2Svg from '../../assets/svgs/steps/step_2.svg';
+import Step3Svg from '../../assets/svgs/steps/step_3.svg';
 
 const StepsSection = () => {
     const chip = 'Our Process, Your Growth'
@@ -27,21 +27,21 @@ const StepsSection = () => {
     }
 
     return (
-        <Section sectionClassName='bg-green-dark-2 py-20 md:py-32' >
-            <div className="max-w-[90rem] mx-auto bg-white p-6 py-12 md:p-8 rounded-4xl md:px-24 md:py-20 my-0 md:my-0">
-                <p className='bg-green-light max-w-fit py-3 px-5 md:px-12 rounded-4xl uppercase mx-auto mb-4 md:mb-7 text-sm md:text-base font-medium text-[#045041] text-center'>
+        <Section sectionClassName='py-20 md:py-32' >
+            <div className="max-w-[90rem] mx-auto bg-[#045041] p-6 py-12 md:p-8 rounded-4xl md:px-24 md:py-20 my-0 md:my-0">
+                <p className='bg-green-light max-w-fit py-3 px-5 md:px-12 rounded-4xl uppercase mx-auto mb-4 md:mb-7 text-sm md:text-base font-medium text-[[#045041]] text-center'>
                     {chip}
                 </p>
-                <h3 className='text-[#045041] text-center font-nohemi font-extrabold text-2xl md:text-5xl md:px-64 mb-5'>
+                <h3 className='text-white text-center font-nohemi font-extrabold text-2xl md:text-5xl md:px-64 mb-5'>
                     {headline}
                 </h3>
-                <p className='text-center mb-8 md:mb-14 text-base md:text-2xl md:px-26'>{lead}</p>
+                <p className='text-center text-white mb-8 md:mb-14 text-base md:text-2xl md:px-26'>{lead}</p>
 
                 <ul className="grid md:grid-cols-3 gap-10 md:gap-5 list-none px-2 md:px-12">
                     {list.map((el, index) => (
                         <motion.li
                             key={index}
-                            className={`${el.className} rounded-3xl w-full p-10`}
+                            className={`bg-white rounded-3xl w-full p-10`}
                             variants={itemVariants}
                             initial="hidden"
                             whileInView="visible"
