1a0c6eac81b43cd79543979925f02c8cb918000f
Arbaj Ansari <arbaj897ansari@gmail.com>
2025-02-27 01:03:54 +0530

    Email Section implemented

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,6 +10,8 @@ import { motion } from "framer-motion";
 import PageTransition from '../../components/layout-ui/PageTransition'
 import StepsSection from './StepsSection'
 import services from '../../data/services'
+import Section from '../../components/layout-ui/Section'
+import { emailAddress } from '../../utils/urls'
 
 const Home = () => {
     const theme = services[1].testimonial.theme
@@ -21,6 +23,24 @@ const Home = () => {
                 <OurWorkSection />
                 <ServicesSection />
                 <StepsSection />
+                <Section>
+                    <h2 className='font-nohemi font-bold md:text-5xl text-2xl text-blue md:text-center mb-6 md:mb-4' >
+                        Didn't find what you're looking for?
+                        <br />
+
+                    </h2>
+                    <p className='md:text-3xl text-xl md:text-center font-medium mb-28' >
+                        <span>
+                            Email us at
+                        </span>
+                        <span className='font-nohemi inline-block ml-2 text-blue transition-all hover:scale-110 relative left-0 hover:left-4'>
+                            <a href={`mailto:${emailAddress}`} className='inline transition underline cursor-pointer'>
+                                {emailAddress}
+                            </a>
+                        </span>
+
+                    </p>
+                </Section>
                 <TestimonialSection theme={theme} />
                 <FaqSection />
                 <FooterSection />
