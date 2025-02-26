b5089869a41aa14ec59184bc0e6f27e6a0f98893
Arbaj Ansari <arbaj897ansari@gmail.com>
2025-02-26 20:26:26 +0530

    s

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -54,7 +54,7 @@ const HeroSection = () => {
                             <p className='text-base md:text-3xl font-medium text-center max-w-4xl mx-auto mb-12 w-2xl' >We help brands create content that gets
                                 recommended, shared, and remembered</p>
                         </div>
-                        <Button endIcon={<img src={Svg} className='md:w-6 md:h-6 w-3 h-3 filter invert relative bottom-0.5 left-1.5' />} variant='primary' size='lg' className='group mx-auto overflow-hidden relative hover:!bg-primary'>
+                        <Button endIcon={<img src={Svg} className=' md:w-6 md:h-6 w-3 h-3 filter invert relative bottom-0.5 left-1.5' />} variant='primary' size='lg' className='shadow-[0px_2px_1px_1px_#00000066] group mx-auto overflow-hidden relative hover:!bg-primary'>
                             <div className="absolute left-[-300%] bottom-[-50%] scale-110  group-hover:bottom-0 group-hover:left-[0%] top-0 w-[300%] opacity-100 group-hover:opacity-100 group-hover:w-[300%] h-full"
                                 style={{
                                     background: 'linear-gradient(45deg, rgba(0, 0, 0, 0.253), rgba(0, 0, 0, 0.253) 40%, transparent 40%, transparent 50%, rgba(0, 0, 0, 0.253) 50%, rgba(0, 0, 0, 0.253) 60%, transparent 60%, transparent 78%, transparent 78%, transparent 100%)',
